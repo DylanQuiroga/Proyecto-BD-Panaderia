a4f3988f0743db35d3f66e322a49ce69bb95c739
Vicente Muñoz <vicente.munoz@userena.cl>
2023-11-22 18:12:13 -0300

    pruebas

--- Panaderia/src/Receta/DBReceta.java
+++ Panaderia/src/Receta/DBReceta.java
@@ -1,5 +1,10 @@
 package Receta;
 
+import java.sql.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author rebv1
@@ -11,5 +16,36 @@ public class DBReceta {
     static String url = "jdbc:postgresql://10.4.3.195:5432/" + dbname;
     static String username = "panaderia";
     static String password = "im7stB6";
+    
+    String retorno[];
+    int i = 0;
+
+    public String[] cargarRecetas() throws SQLException {
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            String consulta = "SELECT * FROM receta";
+            PreparedStatement preparedStatement = connection.prepareStatement(consulta);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                i++;
+            }
+
+            retorno = new String[i];
+            int i = 0;
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                retorno[i] = resultSet.getString("nombre_receta");
+                i++;
+            }
+
+            return retorno;
+
+        } catch (SQLException e) {
+            return null;
+        }
+
+    }
     
 }
