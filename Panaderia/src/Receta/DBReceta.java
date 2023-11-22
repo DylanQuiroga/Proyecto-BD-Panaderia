a5c0533e6dd4d3ce01d1e540947d8af83e825dce
Vicente Muñoz <vicente.munoz@userena.cl>
2023-11-22 16:49:25 -0300

    Create DBReceta.java

---
+++ Panaderia/src/Receta/DBReceta.java
@@ -0,0 +1,15 @@
+package Receta;
+
+/**
+ *
+ * @author rebv1
+ */
+public class DBReceta {
+    
+    static String driver = "org.postgresql.Driver";
+    static String dbname = "panaderia";
+    static String url = "jdbc:postgresql://10.4.3.195:5432/" + dbname;
+    static String username = "panaderia";
+    static String password = "im7stB6";
+    
+}
