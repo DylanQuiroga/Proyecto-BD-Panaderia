6c7c8096fda30cb7cbc5663fc711157c6bb964b2
Ethan Pimentel Cortes <102681829+EthanPimentel@users.noreply.github.com>
2023-11-28 20:28:30 -0300

    Creacion de carpeta

---
+++ Panaderia/src/GUI_Proveedores/DBProveedores.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI_Proveedores;
+
+import GUI_Login.GUI_Login;
+import GUI_Login.GUI_Opciones;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Ethan
+ */
+public class DBProveedores {
+    
+}
