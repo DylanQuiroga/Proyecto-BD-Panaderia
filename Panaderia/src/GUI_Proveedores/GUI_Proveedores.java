96a3af0f0d2e480aab9a8082b62f3932207498e9
Ethan Pimentel Cortes <102681829+EthanPimentel@users.noreply.github.com>
2023-11-28 22:17:20 -0300

    nada realmente

--- Panaderia/src/GUI_Proveedores/GUI_Proveedores.java
+++ Panaderia/src/GUI_Proveedores/GUI_Proveedores.java
@@ -27,16 +27,17 @@ public class GUI_Proveedores extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1201, 688));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1201, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 688, Short.MAX_VALUE)
         );
 
         pack();
